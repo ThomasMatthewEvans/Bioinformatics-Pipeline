0199c8f322ea29fa886acde542004525cd6743ad
Matthew Everson <ThomasMatthewEvans@users.noreply.github.com>
2018-12-26 00:49:00 +0000

    Update gene names to sequence to lengths or ids.py

--- gene names to sequence to lengths or ids.py
+++ gene names to sequence to lengths or ids.py
@@ -1,29 +1,20 @@
 # page 155 of manual Enteres search
 
 # Imports module, signs with registered email
-from Bio import Entrez 
-Entrez.email = "************"
+from Bio import Entrez
+import csv
+Entrez.email = "**********"
 
 #MADE FOR NUCCORE BUT OTHER DBS WORK NOT SO WELL THOUGH
+seqOnly = input("Enter A for all annotations and ID, Length, Sequence, Name and description, Enter B for annotations ID, Length and Sequence Only, Enter C to ommit all the previous annotations and print gene data")
 
-seqOnly = input("Enter A for all annotations or B for or ID, Length and Sequence Only")
-
-db="nuccore"
-halfTerm = "ALS1s4"
-term = "APOE"
-
+db="nuccore"                                        # The database used assigend to the db variable
+term = "APOE"                                       # Term is the variable searched, must be a gene name
 
 #Genome gets the size of genes and other interesting facts, can alternate this and sort it if needed
-#TODO
-#TODO
-#TODO
-#TODO
 #DO THE ABOVE 
 
 # This can be changed to different searches to get different info
-
-
-
 handle = Entrez.egquery(term=term)
 # If more than one entered put in AND between the arguments
 record = Entrez.read(handle)
@@ -38,22 +29,25 @@ record = Entrez.read(handle)
 gi_list = record["IdList"]
 print(gi_list)
 
-
 # This will print all the data from all the ID's accessed above
 gi_str = ",".join(gi_list)
 handle = Entrez.efetch(db=db, id=gi_str, rettype="gb", retmode="text")
 # If you want to look at the raw GenBank ﬁles, you can read from this handle and print out the result:
 text = handle.read()
-print(text)
+if seqOnly =="A":
+    print(text)
+elif seqOnly=="B":
+    print(text)
+else:
+    print("running")
 
 # Sort the data and present it in a more pythonic manner
 
-# p156
+# p156 from bipython cookbook
 from Bio import SeqIO
 handle = Entrez.efetch(db=db, id=gi_str, rettype="gb", retmode="text")
 records = SeqIO.parse(handle, "gb")
 #We can now step through the records and look at the information we are interested in:
-
 
 #This prints the summary annotations only anyway regardless of what is entered
 if seqOnly == "A":
@@ -64,3 +58,16 @@ if seqOnly == "A":
 if seqOnly == "B":
     for record in records:
         print("name ", record.name, "length", len(record), "sequence ", record.seq)
+if seqOnly == "C":
+    for record in records:
+    #the below prints all the details
+        print("%s, length %i, with %i features, description: %s, sequence: %s" % (record.name, len(record), len(record.features), record.description, record.seq))
+
+#TODO
+#make functions for each of the print outputs and theya re called via the user input at the beginning
+#save the outputs as a CSV file
+#create a nother file to input these and look at them
+myFile = open('csvexample3.csv', 'w')       #This block is what opens and saves the csv file
+with myFile:  
+   writer = csv.writer(myFile)
+   writer.writerows(myData)                 #Need to fix the myData variable and assign it to the data
